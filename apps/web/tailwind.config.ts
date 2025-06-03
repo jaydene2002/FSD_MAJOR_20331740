8ba874641862b0f16e7a25ed764e99b2561c9c4f
jaydene2002 <jayden.elazzi2002@gmail.com>
2025-06-03 10:24:11 +1000

    updated changes

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,9 +1,11 @@
 import sharedConfig from "@repo/tailwind-config";
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
   content: ["./src/**/*.tsx", "../../packages/ui/src/**/*.tsx"],
   presets: [sharedConfig],
+  plugins: [typography],
 };
 
 export default config;
