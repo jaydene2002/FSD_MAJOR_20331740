c9608fb6e62a79018f24341905c77ba1450e84f4
jaydene2002 <jayden.elazzi2002@gmail.com>
2025-06-03 20:14:17 +1000

    Dynamic link

--- apps/web/src/config/index.ts
+++ apps/web/src/config/index.ts
@@ -1,3 +1,4 @@
 // File: apps/web/src/config/index.ts
 export const PAGE_LIMIT = 3; // Default page limit
-export const ADMIN_URL = "http://localhost:3002/";
+export const ADMIN_URL =
+  process.env.NEXT_PUBLIC_ADMIN_URL || "http://localhost:3002/";
